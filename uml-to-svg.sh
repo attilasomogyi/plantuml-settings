0dab170cbc8d6513fa4e243a5c1a41b42e70be29
Attila Somogyi <attila.zsolt.somogyi@gmail.com>
2022-08-29 21:16:33 +0200

    ci: add corrent uml to svg script and new svg file

--- uml-to-svg.sh
+++ uml-to-svg.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-set -x 
 
 PLANTUML="$(echo ~)/plantuml/plantuml.jar"
 PLANTUML_SVG_DIAGRAMS_DIR="$(pwd)/docs/source/_static/images"
 PLANTUML_DIAGRAM_FILES="$(pwd)/examples"
 java -jar $PLANTUML -svg -o $PLANTUML_SVG_DIAGRAMS_DIR\
  $PLANTUML_DIAGRAM_FILES || exit 1
-
+ 
 cd "$PLANTUML_SVG_DIAGRAMS_DIR" || exit 1
 TIDY_SETTINGS="\
  -xml\
@@ -17,10 +16,10 @@ TIDY_SETTINGS="\
  --indent true\
  --indent-attributes true\
  --indent-spaces 2\
- --sort-attributes true\
  --vertical-space true\
  --wrap 68\
- --wrap-attributes true"
+ --wrap-attributes true\
+ --quiet true"
 
 SVG_FILES="*.svg"
 
