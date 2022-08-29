c77e7a8be40343fbf3db121f5caf20036fdaea11
Attila Somogyi <attila.zsolt.somogyi@gmail.com>
2022-08-29 20:18:20 +0200

    ci: add new uml to svg script

--- uml-to-svg.sh
+++ uml-to-svg.sh
@@ -1,3 +1,28 @@
 #!/bin/sh
+set -x 
 
-java -jar ~/plantuml/plantuml.jar -svg -o "$(pwd)/docs/source/images/" "$(pwd)/examples/*"
+PLANTUML="$(echo ~)/plantuml/plantuml.jar"
+PLANTUML_SVG_DIAGRAMS_DIR="$(pwd)/docs/source/_static/images"
+PLANTUML_DIAGRAM_FILES="$(pwd)/examples"
+java -jar $PLANTUML -svg -o $PLANTUML_SVG_DIAGRAMS_DIR\
+ $PLANTUML_DIAGRAM_FILES || exit 1
+
+cd "$PLANTUML_SVG_DIAGRAMS_DIR" || exit 1
+TIDY_SETTINGS="\
+ -xml\
+ -modify\
+ -utf8\
+ --hide-comments true\
+ --output-xml true\
+ --indent true\
+ --indent-attributes true\
+ --indent-spaces 2\
+ --sort-attributes true\
+ --vertical-space true\
+ --wrap 68\
+ --wrap-attributes true"
+
+SVG_FILES="*.svg"
+
+tidy $TIDY_SETTINGS $SVG_FILES
+
