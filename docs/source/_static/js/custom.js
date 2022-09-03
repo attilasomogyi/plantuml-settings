4822590c9ee77a6a391009981b39fabbdaf597bf
Attila Somogyi <attila.zsolt.somogyi@gmail.com>
2022-09-03 19:36:33 +0200

    docs(custom): fix setfooter

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -3,7 +3,9 @@ const setFooter = () => {
     let footer = document.querySelector('footer');
     let navigation = footer.querySelector('[role="navigation"]');
     let contentinfo = footer.querySelector('[role="contentinfo"]');
+    footer.removeChild(footer.childNodes)
     footer.appendChild(navigation);
+    footer.appendChild("<hr />");
     footer.appendChild(contentinfo);
 
 }
