c5489b7925979a86039dc92415b4491b006c1b75
Attila Somogyi <attila.zsolt.somogyi@gmail.com>
2022-09-11 21:35:34 +0200

    docs(custom.js): fix selector

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -1,12 +1,12 @@
 $(".way-nav-content").on("swipeleft", function (event) {
-  const prev_url = document.querySelector('rel="prev"').href;
+  const prev_url = document.querySelector("a[rel=prev]").href;
   if (prev_url) {
     document.url = prev_url;
   }
 });
 
 $(".way-nav-content").on("swiperight", function (event) {
-  const next_url = document.querySelector('rel="next"').href;
+  const next_url = document.querySelector("a[rel=next]").href;
   if (next_url) {
     document.url = next_url;
   }
