de8bfe43a3e882c6e4fea9dfa507c2e9a682d855
Attila Somogyi <attila.zsolt.somogyi@gmail.com>
2022-09-11 22:46:59 +0200

    docs(custom.js): remove jquery mobile add new touch library

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -1,13 +1,16 @@
-$(".wy-nav-content").on("swipeleft", function (event) {
-  const prev_url = document.querySelector("a[rel=prev]").href;
-  if (prev_url) {
-    document.url = prev_url;
-  }
-});
+const touchEvents = () => {
+  const content = document.querySelector(".wy-nav-content");
+  content.addEventListener("swiped-left", function (e) {
+    const prev_url = document.querySelector("a[rel=prev]").href;
+    if (prev_url) {
+      window.location.href = prev_url;
+    }
+  });
 
-$(".wy-nav-content").on("swiperight", function (event) {
-  const next_url = document.querySelector("a[rel=next]").href;
-  if (next_url) {
-    document.url = next_url;
-  }
-});
+  content.addEventListener("swiped-right", function (e) {
+    const next_url = document.querySelector("a[rel=next]").href;
+    if (next_url) {
+      window.location.href = next_url;
+    }
+  });
+};
