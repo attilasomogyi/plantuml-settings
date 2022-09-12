77912d90545ee8bd858844efb6277db64afdade5
Attila Somogyi <attila.zsolt.somogyi@gmail.com>
2022-09-12 10:49:21 +0200

    docs: remove swipe and setup singlehtml

--- docs/source/_static/js/custom.js
+++ docs/source/_static/js/custom.js
@@ -1,19 +1,1 @@
-const touchEvents = () => {
-  const content = document.querySelector(".wy-nav-content");
-  content.addEventListener("swiped-left", function (e) {
-    const prev_url = document.querySelector("a[rel=prev]").href;
-    if (prev_url) {
-      window.location.href = prev_url;
-      console.log(window.location.href);
-    }
-  });
 
-  content.addEventListener("swiped-right", function (e) {
-    const next_url = document.querySelector("a[rel=next]").href;
-    if (next_url) {
-      window.location.href = next_url;
-      console.log(window.location.href);
-    }
-  });
-};
-touchEvents;
